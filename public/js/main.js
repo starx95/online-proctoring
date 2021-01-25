ca84cacc6dda53b93c595cab858e9b97766625bc
Nizam Yuseri <55922749+starx95@users.noreply.github.com>
2021-01-26 02:45:13 +0800

    new interface

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,36 @@
+$(function() {
 
-(function ($) {
-    "use strict";
+  'use strict';
 
+  $('.js-menu-toggle').click(function(e) {
 
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+  	var $this = $(this);
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+  	
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+  	if ( $('body').hasClass('show-sidebar') ) {
+  		$('body').removeClass('show-sidebar');
+  		$this.removeClass('active');
+  	} else {
+  		$('body').addClass('show-sidebar');	
+  		$this.addClass('active');
+  	}
 
-        return check;
-    });
+  	e.preventDefault();
 
+  });
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+  // click outisde offcanvas
+	$(document).mouseup(function(e) {
+    var container = $(".sidebar");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ( $('body').hasClass('show-sidebar') ) {
+				$('body').removeClass('show-sidebar');
+				$('body').find('.js-menu-toggle').removeClass('active');
+			}
     }
+	}); 
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
     
 
-})(jQuery);
+});
